96237fb995c2ec5ecc331cf47f7178544326b347
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-14 16:43:51 -0300

    Cria a classe Atividade06

--- mesa-de-trabalho/Program.cs
+++ mesa-de-trabalho/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace mesa_de_trabalho
 {
+    public class Atividade06
+    {
+        public string nome;
+        public int idade;
+        public int anoNascimento;
+
+        //Console.Write("Digite sua idade: ");
+        //var idadeDoUsuario = Console.ReadLine()!;
+
+        //var anoAtual = DateTime.Now.Year;
+
+        //// Utilize a mensagem a seguir como base do retorno esperado:
+        //// Olá João, você nasceu em XXXX.
+        //var anoDoNascimento = anoAtual - int.Parse(idadeDoUsuario);
+        //Console.Write($"Olá {nomeDoUsuario}, você nasceu em {anoDoNascimento}");
+
+        public void Inicio()
+        {
+            PegarNome();
+        }
+
+        public void PegarNome ( )
+        {
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var atividade06 = new Atividade06();
+            atividade06.Inicio();
         }
     }
 }
