ec76f42b6c57d3dc4b55bae0590ef71a33294a5e
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-14 16:09:41 -0300

    Cria o arquivo no Visual Studio

---
+++ mesa-de-trabalho/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mesa_de_trabalho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
